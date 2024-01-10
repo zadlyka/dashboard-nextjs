d2881c839d1ef8704fcf8efc168493adbbcd81e1
zadlyka <tara02zadlyka@gmail.com>
2024-01-10 14:20:18 +0700

    feat(catalog): implement carousel catalog

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 import {
   Table,
   TableBody,
@@ -9,6 +23,8 @@ import {
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import Layout from "@/components/layout";
+import { Album } from "./data/albums";
+import { madeForYouAlbums } from "./data/albums";
 
 export default function Index() {
   return (
@@ -24,6 +40,7 @@ export default function Index() {
         </div>
         <Button>Add</Button>
       </div>
+
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
@@ -43,6 +60,52 @@ export default function Index() {
           </TableRow>
         </TableBody>
       </Table>
+
+      <div className="inline-flex justify-between w-full">
+        <div className="space-y-1">
+          <h2 className="text-xl font-semibold tracking-tight sm:text-2xl">
+            Made for you
+          </h2>
+          <p className="text-xs sm:text-sm text-muted-foreground">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+        </div>
+      </div>
+
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full"
+      >
+        <CarouselContent>
+          {madeForYouAlbums.map((album: Album, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <Image
+                    src={album.cover}
+                    alt={album.name}
+                    width={150}
+                    height={150}
+                    className="object-cover w-full h-full transition-all hover:scale-105 aspect-square"
+                  />
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem>Detail</ContextMenuItem>
+                  <ContextMenuItem>Delete</ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </Layout>
   );
 }
